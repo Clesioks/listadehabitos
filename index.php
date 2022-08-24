229bba4d3ce6cd3ec751a8d46ae28659e1410116
Clesioks <kornalewski@outlook.com>
2022-08-24 19:27:09 -0300

    first

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Lista de hábitos</title>
+</head>
+
+<body>
+    <div class="center">
+        <h1>Lista de hábitos</h1>
+        <p>Cadastre aqui os hábitos que você tem que vencer para melhorar sua vida!</p>
+
+        <?php
+
+        // Obtém a lista de hábitos do
+        // banco de dados MySQL
+
+        $servidor = "localhost";
+        $usuario = "root";
+        $senha = "";
+        $bancodedados = "listadehabitos";
+
+        // Cria uma conexão com o banco de dados
+
+    </div>
+</body>
+
+</html>
